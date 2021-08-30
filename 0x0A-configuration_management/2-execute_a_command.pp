a095af2afa00d8ec51bb3fc2950550c229830925
92546518Mahdi@@ <mahdiabid158@gmail.com>
2021-08-30 12:15:03 +0100

    fix code

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,4 +1,5 @@
-# killing process using puppet
-exec { "kill_process":
-    command => 'pkill killmenow',
+# kills a process named killmenow using Puppet
+exec {'killmenow':
+path    => '/usr/bin',
+command => 'pkill -f killmenow'
 }
