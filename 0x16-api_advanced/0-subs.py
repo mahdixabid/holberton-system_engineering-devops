1516d7635199a75ad09a2b1fbdd116f84c9ec7c9
mahdixabid <mahdiabid158@gmail.com>
2021-09-30 16:24:13 +0200

    fix code and style

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,16 +1,14 @@
 #!/usr/bin/python3
 # queries the Reddit API and returns the number of subscribers
+import json
 import requests
 
 
 def number_of_subscribers(subreddit):
-    """return total subscribers of reddit's subreddit"""
-
-    url = 'https://www.reddit.com/r/' + subreddit + '/about.json'
-    try:
-        response = requests.get(url,
-                                headers={'User-agent': 'your bot 0.1'},
-                                allow_redirects=False).json()
-        return response['data'].get('subscribers')
-    except:
+    user = {"User-Agent": "Maroua-Alaya"}
+    request = requests.get("https://www.reddit.com/r/{}/about.json"
+                           .format(subreddit), headers=user)
+    if request.status_code == 200:
+        return request.json().get("data").get("subscribers")
+    else:
         return 0
