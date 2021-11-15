c3c631b3cc496a478390d695d298f9cda492c291
mahdixabid <mahdiabid158@gmail.com>
2021-11-15 23:25:20 +0100

    fix pep

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """
  using this REST API, for a given employee ID,
- returns information about his/her TODO list progress.
+ returns information about his/her TODO list progress..
 """
 import requests
 from sys import argv
